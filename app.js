ac363ad522c35d70e3c65cc3fefc4819aad38f6b
Linus-Peng1 <snoopypeng1217@gmail.com>
2021-07-19 20:28:40 +0800

    feat: add create record function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const Record = require('./models/record')
 const Category = require('./models/category')
@@ -11,6 +12,9 @@ const app = express()
 
 app.engine('hbs', exphbs({ defaultLayput: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect('mongodb://localhost/expense-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -23,6 +27,22 @@ db.on('error', () => {
 // 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
+})
+
+app.get('/records/new', (req, res) => {
+  Category.find()
+    .lean()
+    .then(categories => res.render('new', { categories }))
+})
+
+app.post('/records', (req, res) => {
+  const { name, category, date, amount } = req.body
+  if (!name || !category || !date || !amount) {
+    return res.redirect('/records/new')
+  }
+  return Record.create({ name, category, date, amount })
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
 })
 
 app.get('/', (req, res) => {
