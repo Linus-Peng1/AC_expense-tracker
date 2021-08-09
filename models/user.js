4e0b28a901cd4ccdd1729f9fe97ed63e88c317d5
Linus-Peng1 <snoopypeng1217@gmail.com>
2021-08-09 14:41:10 +0800

    feat: add user registration feature

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const UserSchema = new Schema({
+const userSchema = new Schema({
   name: {
     type: String,
     required: true
@@ -11,7 +11,7 @@ const UserSchema = new Schema({
     required: true
   },
   password: {
-    type: Number,
+    type: String,
     required: true
   },
   createAt: {
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 // 使用者登入
 router.get('/login', (req, res) => {
   res.render('login')
@@ -16,7 +18,18 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
 
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      res.render('register', { name, email, password, confirmPassword })
+    } else {
+      User.create({ name, email, password })
+        .then(() => { res.redirect('/') })
+        .catch(err => console.log(err))
+    }
+  })
 })
 
 module.exports = router
