caa0c3ae2b46f424fce3f3173ce65eff191f0232
Linus-Peng1 <snoopypeng1217@gmail.com>
2021-08-09 14:50:11 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 const routes = require('./routes/index')
 require('./config/mongoose')
@@ -15,6 +16,13 @@ app.engine('hbs', exphbs({
   helpers: require('./utils/handlebarsHelpers')
 }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false, // if true，每次與使用者互動後，強制把 session 更新到 session store 裡。
+  saveUninitialized: true // 強制將未初始化的 session 存回 session store。
+}))
+
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
